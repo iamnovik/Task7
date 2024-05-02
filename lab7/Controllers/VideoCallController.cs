adb298cb6e94df9283f8ccbe275a520a335f8573
iamnovik <vlad09082003@mail.ru>
2024-05-03 00:39:26 +0300

    fix

--- lab7/Controllers/VideoCallController.cs
+++ lab7/Controllers/VideoCallController.cs
@@ -4,6 +4,7 @@ namespace lab7.Controllers;
 
 public class VideoCallController : Controller
 {
+    [HttpGet("{chatId}")]
     public IActionResult Index(string chatId)
     {
         ViewBag.ChatId = chatId;
