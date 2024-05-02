994b05914517d9cb3eb94928c0d4ac60d295ea5b
iamnovik <vlad09082003@mail.ru>
2024-05-03 00:22:38 +0300

    another check

--- lab7/Controllers/WebRTCHub.cs
+++ lab7/Controllers/WebRTCHub.cs
@@ -10,22 +10,22 @@ public class WebRTCHub : Hub
         await Clients.Others.SendAsync("Receive", message);
     }
     
-    /*public async Task Send(Data data)
+    public async Task Send(Data data)
     {
         System.Console.WriteLine(data.id +" send");
-        await Clients.Others.SendAsync("ReceiveMessage", data.message);
+        await Clients.Group(data.id).SendAsync("Receive", data.message);
        // await Clients.Group(data.id).SendAsync("ReceiveMessage", data.message);
-    }*/
+    }
 
     public async Task JoinChatGroup(string chatId)
     {
         Console.WriteLine("group"+chatId);
-        //await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
     }
 
     public async Task LeaveChatGroup(string chatId)
     {
-        //await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId.ToString());
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, chatId);
     }
 }
 
