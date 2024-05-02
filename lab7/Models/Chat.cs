ed950bfa2aa22ff97e245c1d6bd8c0c14d2d6f7e
iamnovik <vlad09082003@mail.ru>
2024-05-02 18:09:44 +0300

    fix

---
+++ lab7/Models/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace lab7.Models;
+
+public class Chat
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; } 
+    public string SenderId { get; set; }
+    public string ReceiverId { get; set; }
+
+    public virtual ICollection<Message> Messages { get; set; }
+}
